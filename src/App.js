d499b39133adfc0b6b523ba05c28e1e64054ced8
ARCHXTEKK <slaughterer_archxtekk@mail.ru>
2023-06-12 23:34:54 +0500

    added logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './css/index.css';
 import Question from './Question';
 
@@ -9,9 +9,39 @@ function App() {
     new Question('What note is third in D# minor scale?', 2, ['C#', 'F#', 'Ab', 'la']),
     new Question('How many strings most part of acoustic guitars have?', 4, ['4 or 5 strings', '7 or 8 strings', '9 or 8 strings', '6 or 12 strings'])
   ];
+  const [finished, setFinished] = useState(false);
+  const [answered, setAnswered] = useState(0);
+  const [passed, setPassed] = useState(0);
+
+  const handleAnwseredIncrementOnClick = (event) => {
+    if(QUESTIONS[answered].answer == event.target.value) {
+      setPassed(passed + 1);
+    }
+    setAnswered(answered + 1);
+    if(answered === QUESTIONS.length - 1) {
+      setFinished(true);
+    }
+  }
+
   return (
     <div className="App">
-      
+      {finished ? 
+      <div className='finish'>
+        Congratulations! You finished a quiz with score: {passed}
+      </div> 
+      :
+      <div className='quiz'> 
+        <div className='question'>
+          <div className='question__counter'>{answered + 1}<b>/</b>{QUESTIONS.length}</div>
+          <div className='question__text'>{QUESTIONS[answered].text}</div>
+        </div>
+        <div className='answer'> 
+          {QUESTIONS[answered].options.map((el, index) => {
+            return <button className='answer__option' id={`anwser-${index}`} value={index + 1} onClick={handleAnwseredIncrementOnClick}>{el}</button>
+          })}
+        </div>
+      </div>
+      }
     </div>
   );
 }
--- src/Question.js
+++ src/Question.js
@@ -5,33 +5,31 @@ class ToleranceRangeError extends Error {
     }
 }
 
-class Option {
-    constructor(text, iscorrect=false) {
-        this.text = text;
-        this.iscorrect = iscorrect;
-    }
-}
-
  export default class Question {
     // text - not empty string
     // anwser - number from 0 to options.length
     // options - string or number array with at least 2 elements
-    constructor(text, anwser, options = []) {
+    constructor(text, answer, options = []) {
         if(!text) {
             throw new ToleranceRangeError('argument 1 must be not empty.');
         }
         else if (options.length < 2) {
             throw new ToleranceRangeError('argument 3 must be array and have at least 2 elements');
         }
-        else if (anwser <= 0 || anwser > options.length) {
+        else if (answer <= 0 || answer > options.length) {
             throw new ToleranceRangeError('argument 2 must be higher than zero and lower than argument 3 length.');
         }
-        
         this.text = text;
-        this.anwser = anwser - 1;
-        this.options = [];
-        for(let i = 1; i <= 4; i++) {
-            this.options.push(new Option(options[i], (i === this.anwser) ? true : false ))
-        }
+        this.answer = answer;
+        this.options = options;
     }
+    /*get text() {
+        return this.text;
+    }
+    get options() {
+        return this.options;
+    }
+    get answer() {
+        return this.answer;
+    }*/
 } 
