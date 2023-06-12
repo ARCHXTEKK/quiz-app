a413bea44ada907af3a6cf31d0114947cb1ec4c9
ARCHXTEKK <slaughterer_archxtekk@mail.ru>
2023-06-12 21:29:13 +0500

    added question logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './css/index.css';
+import Question from './Question';
 
 function App() {
+  const QUESTIONS = [
+    new Question('What is the capital of France?', 1, ['Paris', 'Berlin', 'Moscow', 'Korkino']),
+    new Question('How many fingers human have?', 3, ['4', '-8', '5', 'undefined']),
+    new Question('What note is third in D# minor scale?', 2, ['C#', 'F#', 'Ab', 'la']),
+    new Question('How many strings most part of acoustic guitars have?', 4, ['4 or 5 strings', '7 or 8 strings', '9 or 8 strings', '6 or 12 strings'])
+  ];
   return (
     <div className="App">
       
